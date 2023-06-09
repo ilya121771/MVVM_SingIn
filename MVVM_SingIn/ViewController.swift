32d23f7be48f9aa5b42d49494892f06c4a0fbcc6
ilya121771 <Zablotskij98@gmail.com>
2023-06-09 19:29:21 +0300

    Initial Commit

---
+++ MVVM_SingIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_SingIn
+//
+//  Created by Илья on 9.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
